98f7872b1f44e7b884d92884896a2f9d6ed74a71
ScStew <scottmstewart2@gmail.com>
2017-07-27 11:39:36 -0400

    added login page

--- app.rb
+++ app.rb
@@ -1,5 +1,29 @@
 require "sinatra"
 
-get '/' do 
-erb :login
+get '/' do
+message = params[:message] 
+erb :login, locals:{message:message}
+end
+
+post '/login' do
+	correct_login = {scstew: "12345"}
+	username = params[:username]
+	password = params[:password]
+	correct_login.each_pair do |user, value|
+		if username == user.to_s && password == value
+			redirect '/test_page'
+		elsif username == user.to_s
+			message = "incorrect password"
+			redirect '/?message=' + message
+		elsif password == value
+			message = "incorrect username"
+			redirect '/?message=' + message
+		end
+	end
+		message = "incorrect username and password"
+		redirect '/?message=' + message			
+end
+
+get '/test_page' do
+	erb :test_page
 end
